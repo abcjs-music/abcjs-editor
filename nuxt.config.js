6208e8ee12fd10eddc5bea2f8c7590fa1a817719
Markus Hoffrogge <mhoffrogge@gmail.com>
2025-01-29 10:40:37 +0100

    nuxt.config.js: Fix icon link urls to work for static site deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,10 @@ export default {
 			{hid: 'description', name: 'description', content: pkg.description}
 		],
 		link: [
-			{rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'},
-			{rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'},
-			{rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'},
-			{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?x=2'},
+			{rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png'},
+			{rel: 'icon', type: 'image/png', sizes: '32x32', href: 'favicon-32x32.png'},
+			{rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicon-16x16.png'},
+			{rel: 'icon', type: 'image/x-icon', href: 'favicon.ico?x=2'},
 			{
 				rel: 'stylesheet',
 				href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Kreon|Itim|Fira+Mono'
